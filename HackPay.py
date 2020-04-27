10d2b7ff106b8adddb21d83fe160997c7cfcba2e
Sahasaurav565 <58784327+Sahasaurav565@users.noreply.github.com>
2020-04-28 02:36:20 +0530

    Update HackPay.py

--- HackPay.py
+++ HackPay.py
@@ -32,7 +32,7 @@ async def _(event):
             "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking...100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay $69 To `ɦεαrt hαckєr💞💖` To Remove This Hack`" 
+            "`Targeted Account Hacked...\n\nPay $69 To `pєtєr pαrkєr` To Remove This Hack`" 
        ]
 
     for i in animation_ttl:
